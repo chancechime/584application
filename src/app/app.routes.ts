d252cf8ace3915a2a62e859efc744ddab463ea0a
Chance Chime <chance.chime.814@my.csun.edu>
2024-09-20 16:59:52 -0700

    Refactor app routes to include WeatherComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CountryComponent } from './country/country.component';
 import { CityComponent } from './city/city.component';
+import { WeatherComponent } from './weather/weather.component';
 
 export const routes: Routes = [
     {path: "countries", component: CountryComponent},
-    {path: "cities", component: CityComponent}
+    {path: "cities", component: CityComponent},
+    {path: "", component: WeatherComponent, pathMatch: "full"}
 ];
