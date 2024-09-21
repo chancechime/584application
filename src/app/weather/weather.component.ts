766022124277e3897fb9230925d0027be240d901
Chance Chime <chance.chime.814@my.csun.edu>
2024-09-20 17:26:55 -0700

    Refactor app routes to include WeatherComponent and update environment configuration

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 
 interface WeatherForecast {
@@ -26,7 +27,7 @@ export class WeatherComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('http://localhost:5089/weatherforecast').subscribe(
+    this.http.get<WeatherForecast[]>(`${environment.baseUrl}/weatherForecast`).subscribe(
       (result) => {
         this.forecasts = result;
       },
